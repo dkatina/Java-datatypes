45407578299d55389b1dd1fbec73347e8a3e3f55
dkatina <108095842+dkatina@users.noreply.github.com>
2025-02-06 12:53:27 -0500

    exercises 1 and 2

---
+++ src/exerciseOne.java
@@ -0,0 +1,12 @@
+public class exerciseOne {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 5000;
+        int i = 100000;
+        long l = 1000000000L;
+        float f = 3.14f;
+        double d = 10.12345;
+        char c = 'C';
+        boolean isJavaFun = true;
+    }
+}
---
+++ src/exerciseTwo.java
@@ -0,0 +1,11 @@
+public class exerciseTwo {
+    public static void main(String[] args) {
+//        int $price;
+//        int 5thEdition;
+//        int user_name;
+//        int totalAmount$;
+//        int final;
+//        int MaxValue;
+//        int @discount;
+    }
+}
