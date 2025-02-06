2d89691ad03369bfcc9568dba43a28aa257ea758
dkatina <108095842+dkatina@users.noreply.github.com>
2025-02-06 12:08:06 -0500

    Declaring and Assigning variables separately

---
+++ src/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Declaration
+        // Declaring variable and it's type without setting it equal to a val TYPE IS REQUIRED
+        int age;
+        String name;
+
+        //Assign declared variables to value
+        age = 26;
+        name = "Dylan";
+    }
+}
